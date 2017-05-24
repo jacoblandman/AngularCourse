534eea88893d34c41d81621e54ce911ad8b402da
Jacob Landman <jakeypoolandman7@gmail.com>
2017-05-24 14:15:03 -0500

    Finished basic setup of the course project

---
+++ course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'http://technicallyteamann.com/wp-content/uploads/2017/03/Recipe_logo-1.jpeg'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'http://technicallyteamann.com/wp-content/uploads/2017/03/Recipe_logo-1.jpeg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
