ffef97791160715461be28ffc445a9ce8234af95
Jacob Landman <jakeypoolandman7@gmail.com>
2017-05-25 11:24:03 -0500

    Added increased functionality to the course project by passing data between components

--- course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -10,12 +10,18 @@ export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'http://technicallyteamann.com/wp-content/uploads/2017/03/Recipe_logo-1.jpeg'),
-    new Recipe('A Test Recipe', 'This is simply a test', 'http://technicallyteamann.com/wp-content/uploads/2017/03/Recipe_logo-1.jpeg')
+    new Recipe('A Test Recipe 2', 'This is simply a test 2', 'http://technicallyteamann.com/wp-content/uploads/2017/03/Recipe_logo-1.jpeg')
   ];
+
+  @Output() recipeClick = new EventEmitter<Recipe>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeClick(recipe: Recipe) {
+    this.recipeClick.emit(recipe);
   }
 
 }
--- course-project/src/app/shopping-list/shopping-list.component.ts
+++ course-project/src/app/shopping-list/shopping-list.component.ts
@@ -17,4 +17,8 @@ export class ShoppingListComponent implements OnInit {
   ngOnInit() {
   }
 
+  onIngredientAdded(ingredent: Ingredient) {
+    this.ingredients.push(ingredent);
+  }
+
 }
