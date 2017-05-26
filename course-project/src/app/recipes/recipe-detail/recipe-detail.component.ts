bdd66061522e7f1aa0d6b63f0e7a924aca82af32
Jacob Landman <jakeypoolandman7@gmail.com>
2017-05-26 11:58:50 -0500

    Added routing to the course project instead of using ngIf

--- course-project/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ course-project/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from "../recipe.service";
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -8,14 +9,28 @@ import { RecipeService } from "../recipe.service";
   styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
-  @Input() recipe: Recipe;
-  constructor(private recipeServer: RecipeService) { }
+  recipe: Recipe;
+  id: number;
+
+  constructor(private recipeServer: RecipeService,
+              private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.id = +params['id'];
+        this.recipe = this.recipeServer.getRecipe(this.id);
+      }
+    );
   }
 
   onAddToShoppingList() {
     this.recipeServer.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+
+  onEditRecipe() {
+    this.router.navigate(['edit'], {relativeTo: this.route});
   }
 
 }
